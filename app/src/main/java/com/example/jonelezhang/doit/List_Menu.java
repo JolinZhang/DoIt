fc561b4104453463fb41a8790aa7b0d294b1d3f0
Jolin Zhang <zhangru0026@gmail.com>
2016-07-13 00:00:31 -0500

    study about EventMotion

--- app/src/main/java/com/example/jonelezhang/doit/List_Menu.java
+++ app/src/main/java/com/example/jonelezhang/doit/List_Menu.java
@@ -1,17 +1,23 @@
 package com.example.jonelezhang.doit;
 
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class List_Menu extends AppCompatActivity {
-
+public class List_Menu extends AppCompatActivity implements OnTouchListener {
+    List_Recycler_View_Adapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +26,13 @@ public class List_Menu extends AppCompatActivity {
         List<ListData> data = fill_with_data();
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list_recyclerView);
         //get instance of list adapter
-        List_Recycler_View_Adapter adapter = new List_Recycler_View_Adapter(data,getApplication());
+        adapter = new List_Recycler_View_Adapter(data,getApplication());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
+        recyclerView.setOnTouchListener(this);
     }
+
+    //initial listData
     public List<ListData> fill_with_data() {
 
         List<ListData> data = new ArrayList<>();
@@ -53,5 +61,32 @@ public class List_Menu extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int action = MotionEventCompat.getActionMasked(event);
+
+        switch(action) {
+//            case (MotionEvent.ACTION_DOWN) :
+//                Toast.makeText(getApplicationContext(), "Action was down ", Toast.LENGTH_SHORT).show();
+//                return true;
+//            case (MotionEvent.ACTION_MOVE) :
+//                Toast.makeText(getApplicationContext(), "Action was MOVE ", Toast.LENGTH_SHORT).show();
+//                return true;
+            case (MotionEvent.ACTION_UP) :
+                adapter.insert(0,new ListData("",""));
+//                Toast.makeText(getApplicationContext(), "Action was UP ", Toast.LENGTH_SHORT).show();
+                return true;
+//            case (MotionEvent.ACTION_CANCEL) :
+//                Toast.makeText(getApplicationContext(), "Action was CANCEL ", Toast.LENGTH_SHORT).show();
+//                return true;
+//            case (MotionEvent.ACTION_OUTSIDE) :
+//                Log.d("DEBUG_TAG","Movement occurred outside bounds " +
+//                        "of current screen element");
+//                return true;
+            default :
+                return super.onTouchEvent(event);
+        }
     }
 }
